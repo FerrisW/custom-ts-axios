c55f1b515768ae61a0765f4d23e9b6fc2e297097
FerrisW <1360030912@qq.com>
2022-03-18 09:26:47 +0800

    feat: handle error situation

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,16 +1,21 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { url, method = 'get', data = null, params, headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { url, method = 'get', data = null, headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {  //单位为毫秒
+      request.timeout = timeout
+    }
     request.open(method.toUpperCase(), url, true)
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) return
+      if (request.status === 0) return
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
@@ -21,7 +26,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
+    }
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request))
+    }
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request)); //专业术语：ECONNABORTED
     }
     Object.keys(headers).forEach(name => {
       if (data === null && name.toLowerCase() === 'content-type') {
@@ -31,5 +42,12 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     })
     request.send(data)
+    function handleResponse(response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(createError(`Request failed with status code ${response.status}`, config, null, request, response))
+      }
+    }
   })
 }
